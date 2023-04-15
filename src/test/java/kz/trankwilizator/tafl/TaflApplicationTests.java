2712f86d3a3d622b83f79e9cd7facf16302e168a
maksim1708 <musiev_maksim@mail.ru>
2023-04-15 19:52:31 +0400

    initial commit

---
+++ src/test/java/kz/trankwilizator/tafl/TaflApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.trankwilizator.tafl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaflApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
