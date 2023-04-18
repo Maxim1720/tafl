c9e1297b77ccda4555620993dbad02d91970479c
maksim1708 <musiev_maksim@mail.ru>
2023-04-19 00:08:58 +0400

    to create UserRepository

--- src/main/java/kz/trankwilizator/tafl/repository/UserRepository.java
+++ src/main/java/kz/trankwilizator/tafl/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package kz.trankwilizator.tafl.repository;
 
-import kz.trankwilizator.tafl.entity.User;
+import kz.trankwilizator.tafl.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
