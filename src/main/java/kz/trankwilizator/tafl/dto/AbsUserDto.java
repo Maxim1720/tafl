be4e484db60ce8a1e9a321affd61842c0f42ac45
Maxim1720 <musiev17@gmail.com>
2023-04-19 23:14:12 +0400

    to add 'id' field to AbsUserDto and set JsonProperty(access=READ_ONLY)

--- src/main/java/kz/trankwilizator/tafl/dto/AbsUserDto.java
+++ src/main/java/kz/trankwilizator/tafl/dto/AbsUserDto.java
@@ -1,9 +1,18 @@
 package kz.trankwilizator.tafl.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class AbsUserDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
     private String username;
     private Double balance;
 }
