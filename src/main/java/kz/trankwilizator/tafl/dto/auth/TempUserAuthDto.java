a09dac736021a4975fd5880b220f3cf25dba1e4c
Maxim1720 <musiev17@gmail.com>
2023-04-19 20:49:24 +0400

    to add @Getter @Setter @ToString on TempUserAuthDto class

--- src/main/java/kz/trankwilizator/tafl/dto/auth/TempUserAuthDto.java
+++ src/main/java/kz/trankwilizator/tafl/dto/auth/TempUserAuthDto.java
@@ -1,5 +1,12 @@
 package kz.trankwilizator.tafl.dto.auth;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class TempUserAuthDto extends UserAuthDto{
 
 }
