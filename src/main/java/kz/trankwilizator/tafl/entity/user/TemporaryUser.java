54fc4c49d9f3e44656cfd2cf3ff746bd8af078f2
Maxim1720 <musiev17@gmail.com>
2023-04-19 23:16:50 +0400

    to set column of TemporaryUser from field to get method and calculate expiry date from current time for

--- src/main/java/kz/trankwilizator/tafl/entity/user/TemporaryUser.java
+++ src/main/java/kz/trankwilizator/tafl/entity/user/TemporaryUser.java
@@ -4,11 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 @Entity(name = "temp_user")
 @Table(name = "temporary_user")
 public class TemporaryUser extends AbsUser{
+
+    private Date expiryAt;
+
     @Column(nullable = false)
-    private LocalDateTime expiryAt;
+    public Date expiryAt(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY, 24);
+        return Date.from(calendar.toInstant());
+    }
 }
