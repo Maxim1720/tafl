889454b8d20b6058061083ac16a62312bd35d6da
Maxim1720 <musiev17@gmail.com>
2023-04-19 23:08:31 +0400

    to add UserDetailsService, PasswordEncoder, DaoAuthenticationProvider to http security configuration, WHITELIST_URLS for get auth and swagger end-points access

--- src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
+++ src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
@@ -1,33 +1,54 @@
 package kz.trankwilizator.tafl.config;
 
 import kz.trankwilizator.tafl.auth.AuthService;
-import kz.trankwilizator.tafl.auth.RegistrationService;
-import kz.trankwilizator.tafl.auth.TemporaryUserService;
-import kz.trankwilizator.tafl.dto.TemporaryUserDto;
+import kz.trankwilizator.tafl.auth.reg.RegistrationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    public static String[] WHITELIST_URLS={"/v3/api-docs/**","/swagger-ui/**", "/auth/**"};
+    private final UserDetailsService userDetailsService;
 
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf()
                 .disable()
                 .authorizeHttpRequests(
-                        (r)-> r.requestMatchers("/v3/api-docs/**","/swagger-ui/**")
+                        (r)-> r.requestMatchers(WHITELIST_URLS)
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
+                .authenticationProvider(authenticationProvider())
+                .userDetailsService(userDetailsService)
                 .httpBasic()
                 .and()
                 .build();
     }
 
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
 
     @Bean
     public RegistrationService registrationService(){
