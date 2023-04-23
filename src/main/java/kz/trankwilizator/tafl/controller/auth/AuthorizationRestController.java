001366baed09df5d22e9a95c43ac5032d80e1c5c
maksim1708 <musiev_maksim@mail.ru>
2023-04-24 02:07:07 +0400

    to rename AuthorizationController to AuthorizationRestController and remove annotations

--- src/main/java/kz/trankwilizator/tafl/controller/auth/AuthorizationRestController.java
+++ src/main/java/kz/trankwilizator/tafl/controller/auth/AuthorizationRestController.java
@@ -6,14 +6,10 @@ import kz.trankwilizator.tafl.dto.AuthToken;
 import kz.trankwilizator.tafl.dto.auth.UserAuthDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth/login")
-public abstract class AuthorizationController<UserDto extends UserAuthDto> {
+public abstract class AuthorizationRestController<UserDto extends UserAuthDto> {
     private final AuthService<UserDto> authService;
-    public AuthorizationController(AuthService<UserDto> authService) {
+    protected AuthorizationRestController(AuthService<UserDto> authService) {
         this.authService = authService;
     }
 
