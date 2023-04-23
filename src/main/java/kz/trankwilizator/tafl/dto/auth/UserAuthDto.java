0baf6008f62b117a3bd38af46a78bfdafaa3a517
maksim1708 <musiev_maksim@mail.ru>
2023-04-24 02:03:04 +0400

    to annotate UserAuthDto.createdAt by @JsonProperty(access = JsonProperty.Access.READ_ONLY)

--- src/main/java/kz/trankwilizator/tafl/dto/auth/UserAuthDto.java
+++ src/main/java/kz/trankwilizator/tafl/dto/auth/UserAuthDto.java
@@ -1,5 +1,6 @@
 package kz.trankwilizator.tafl.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -7,5 +8,6 @@ import java.util.Date;
 @Data
 public abstract class UserAuthDto {
     private String username;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date createdAt;
 }
