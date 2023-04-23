30bca75f7605185b4f1c8997a1e874deaa1bbb70
maksim1708 <musiev_maksim@mail.ru>
2023-04-24 02:08:24 +0400

    to rename permanentUserRegistrationService field

--- src/main/java/kz/trankwilizator/tafl/controller/auth/reg/RegistrationRestController.java
+++ src/main/java/kz/trankwilizator/tafl/controller/auth/reg/RegistrationRestController.java
@@ -1,7 +1,7 @@
 package kz.trankwilizator.tafl.controller.auth.reg;
 
-import kz.trankwilizator.tafl.auth.reg.RegistrationService;
-import kz.trankwilizator.tafl.auth.reg.RegistrationTemporaryUserService;
+import kz.trankwilizator.tafl.auth.reg.PermanentUserRegistrationService;
+import kz.trankwilizator.tafl.auth.reg.temp.RegistrationTemporaryUserService;
 import kz.trankwilizator.tafl.dto.TemporaryUserDto;
 import kz.trankwilizator.tafl.dto.PermanentUserDto;
 import org.springframework.http.HttpStatus;
@@ -14,18 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth/reg")
 public class RegistrationRestController {
-    private final RegistrationService registrationService;
+    private final PermanentUserRegistrationService permanentUserRegistrationService;
     private final RegistrationTemporaryUserService registrationTemporaryUserService;
 
-    public RegistrationRestController(RegistrationService registrationService,
+    public RegistrationRestController(PermanentUserRegistrationService permanentUserRegistrationService,
                                       RegistrationTemporaryUserService registrationTemporaryUserService) {
-        this.registrationService = registrationService;
+        this.permanentUserRegistrationService = permanentUserRegistrationService;
         this.registrationTemporaryUserService = registrationTemporaryUserService;
     }
 
     @PostMapping("/permanent")
     public ResponseEntity<PermanentUserDto> createUser(@RequestBody PermanentUserDto user){
-        return ResponseEntity.status(HttpStatus.CREATED).body(registrationService.createUser(user));
+        return ResponseEntity.status(HttpStatus.CREATED).body(permanentUserRegistrationService.createUser(user));
     }
     @PostMapping("/temp")
     public ResponseEntity<TemporaryUserDto> createCheck(){
