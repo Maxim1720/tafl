42bc3793901b3114100449ecc8c75404b68a4047
maksim1708 <musiev_maksim@mail.ru>
2023-04-23 22:10:07 +0400

    to remove unnecessary TemporaryUserService.java

--- src/main/java/kz/trankwilizator/tafl/auth/TemporaryUserService.java
+++
@@ -1,8 +0,0 @@
-package kz.trankwilizator.tafl.auth;
-
-import kz.trankwilizator.tafl.dto.TemporaryUserDto;
-
-public interface TemporaryUserService {
-    TemporaryUserDto create();
-    TemporaryUserDto replenishBalance(Double amount);
-}
