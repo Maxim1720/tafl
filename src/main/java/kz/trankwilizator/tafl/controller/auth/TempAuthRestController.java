9ac48bcd3b6062f8e6e3e97aaa4c37d0d9405049
maksim1708 <musiev_maksim@mail.ru>
2023-04-24 02:05:04 +0400

    to remove @PostMapping("/temp") from TempAuthRestController.authorization

--- src/main/java/kz/trankwilizator/tafl/controller/auth/TempAuthRestController.java
+++ src/main/java/kz/trankwilizator/tafl/controller/auth/TempAuthRestController.java
@@ -3,11 +3,10 @@ package kz.trankwilizator.tafl.controller.auth;
 
 import kz.trankwilizator.tafl.auth.AuthService;
 import kz.trankwilizator.tafl.dto.AuthToken;
-import kz.trankwilizator.tafl.dto.TemporaryUserDto;
 import kz.trankwilizator.tafl.dto.auth.TempUserAuthDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +15,6 @@ public class TempAuthRestController extends AuthorizationRestController<TempUser
     public TempAuthRestController(AuthService<TempUserAuthDto> authService) {
         super(authService);
     }
-
-    @PostMapping("/temp")
     @Override
     public ResponseEntity<AuthToken> authorization(@RequestBody TempUserAuthDto user) {
         return super.authorization(user);
