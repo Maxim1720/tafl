320ffbf9e4da7001646f808323cd02cbd8415196
maksim1708 <musiev_maksim@mail.ru>
2023-04-24 02:10:06 +0400

    to add filter before-filter

--- src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
+++ src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
