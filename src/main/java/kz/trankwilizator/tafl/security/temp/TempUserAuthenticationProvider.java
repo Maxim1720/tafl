ba616f49580ec62acb2da530b38aed6bd46ddb58
maksim1708 <musiev_maksim@mail.ru>
2023-04-25 09:30:03 +0400

    to rm unused imports

--- src/main/java/kz/trankwilizator/tafl/security/temp/TempUserAuthenticationProvider.java
+++ src/main/java/kz/trankwilizator/tafl/security/temp/TempUserAuthenticationProvider.java
@@ -4,13 +4,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class TempUserAuthenticationProvider implements AuthenticationProvider {
