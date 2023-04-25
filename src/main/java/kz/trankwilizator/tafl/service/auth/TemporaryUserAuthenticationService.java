52b8c20f7e4694933f0b12392619acf25eb9ca1c
maksim1708 <musiev_maksim@mail.ru>
2023-04-25 09:21:15 +0400

    to change import

--- src/main/java/kz/trankwilizator/tafl/service/auth/TemporaryUserAuthenticationService.java
+++ src/main/java/kz/trankwilizator/tafl/service/auth/TemporaryUserAuthenticationService.java
@@ -4,7 +4,7 @@ import kz.trankwilizator.tafl.entity.user.TemporaryUser;
 import kz.trankwilizator.tafl.service.crud.JwtTokenCrudService;
 import kz.trankwilizator.tafl.dto.auth.TempUserAuthDto;
 import kz.trankwilizator.tafl.security.JwtTokenProvider;
-import kz.trankwilizator.tafl.service.JwtTokenCreator;
+import kz.trankwilizator.tafl.service.util.JwtTokenCreator;
 import kz.trankwilizator.tafl.service.crud.user.UserCrudService;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
