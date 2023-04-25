b0d3e63785c2d5cab2e8c6ec54cd650e2a7cfbc3
maksim1708 <musiev_maksim@mail.ru>
2023-04-25 09:03:28 +0400

    to create authentication services

---
+++ src/main/java/kz/trankwilizator/tafl/service/auth/PermanentUserAuthenticationService.java
@@ -0,0 +1,22 @@
+package kz.trankwilizator.tafl.service.auth;
+
+import kz.trankwilizator.tafl.dto.auth.PermanentUserAuthDto;
+import kz.trankwilizator.tafl.entity.user.PermanentUser;
+import kz.trankwilizator.tafl.security.JwtTokenProvider;
+import kz.trankwilizator.tafl.service.JwtTokenCreator;
+import kz.trankwilizator.tafl.service.crud.JwtTokenCrudService;
+import kz.trankwilizator.tafl.service.crud.user.UserCrudService;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+@Log
+@Service
+public class PermanentUserAuthenticationService extends AuthenticationService<PermanentUserAuthDto, PermanentUser> {
+
+    public PermanentUserAuthenticationService(JwtTokenCrudService jwtTokenCrudService,
+                                              JwtTokenCreator jwtTokenCreator,
+                                              JwtTokenProvider jwtTokenProvider,
+                                              UserCrudService<PermanentUser> userUserCrudService) {
+        super(jwtTokenCrudService, jwtTokenCreator, jwtTokenProvider, userUserCrudService);
+    }
+}
