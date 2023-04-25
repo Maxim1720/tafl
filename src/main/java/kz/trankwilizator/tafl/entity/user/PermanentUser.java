0a9792e4d26ea30e2a27124f3a9c4aa576878f30
maksim1708 <musiev_maksim@mail.ru>
2023-04-25 10:29:39 +0400

    to create PermanentUser

---
+++ src/main/java/kz/trankwilizator/tafl/entity/user/PermanentUser.java
@@ -0,0 +1,51 @@
+package kz.trankwilizator.tafl.entity.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import kz.trankwilizator.tafl.entity.role.Role;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "user_profile")
+@Table(name = "user_profile")
+public class PermanentUser extends User implements Serializable {
+
+    @Column(length = 75, nullable = false)
+    private String firstname;
+
+    @Column(length = 75, nullable = false)
+    private String lastname;
+
+    @Column(length = 75)
+    private String secondName;
+
+    @Column(length = 256, nullable = false)
+    private String email;
+
+    @Column(length = 200, nullable = false)
+    private String password;
+
+    @DecimalMin(value = "0")
+    @DecimalMax(value = "1")
+    @Column(columnDefinition = "decimal(1,2)", nullable = false)
+    private Double discount = 0.0;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Date updatedAt;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+}
