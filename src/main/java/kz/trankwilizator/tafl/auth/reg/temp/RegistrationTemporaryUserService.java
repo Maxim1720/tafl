22c272e6a3859936996c5d6923b13ddc82ecad4b
maksim1708 <musiev_maksim@mail.ru>
2023-04-25 09:17:29 +0400

    to rename TemporaryUserRegistrationService

--- src/main/java/kz/trankwilizator/tafl/auth/reg/temp/RegistrationTemporaryUserService.java
+++
@@ -1,7 +0,0 @@
-package kz.trankwilizator.tafl.auth.reg.temp;
-
-import kz.trankwilizator.tafl.dto.TemporaryUserDto;
-
-public interface RegistrationTemporaryUserService {
-    TemporaryUserDto create();
-}
