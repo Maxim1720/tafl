e957a11d5923986469eab72a2d6dabe1d2a1eacc
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:47:19 +0400

    to rename TempUserDetailsService to TemporaryUserDetailsService and add constructor with arg for calling super constructor

--- src/main/java/kz/trankwilizator/tafl/security/details/TemporaryUserDetailsService.java
+++ src/main/java/kz/trankwilizator/tafl/security/details/TemporaryUserDetailsService.java
@@ -1,22 +1,20 @@
 package kz.trankwilizator.tafl.security.details;
 
-import kz.trankwilizator.tafl.dao.user.UserRepository;
-import kz.trankwilizator.tafl.security.JwtTokenProvider;
-import kz.trankwilizator.tafl.service.crud.TemporaryUserCrudService;
 import kz.trankwilizator.tafl.entity.user.TemporaryUser;
+import kz.trankwilizator.tafl.service.crud.user.UserCrudService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
 @Service
-public class TempUserDetailsService extends AbsUserDetailsService {
+public class TemporaryUserDetailsService extends AbsUserDetailsService<TemporaryUser> {
+
+    public TemporaryUserDetailsService(UserCrudService<TemporaryUser> temporaryUserUserCrudService) {
+        super(temporaryUserUserCrudService);
+    }
 
     @Override
     protected List<GrantedAuthority> authorities(String username) {
