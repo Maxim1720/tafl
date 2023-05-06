80a0f3eedd5c2b83d29a5e7b6c67d9b9680d716b
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:50:08 +0400

    to create PermanentRegResponseDto

---
+++ src/main/java/kz/trankwilizator/tafl/dto/response/PermanentRegResponseDto.java
@@ -0,0 +1,19 @@
+package kz.trankwilizator.tafl.dto.response;
+
+import kz.trankwilizator.tafl.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermanentRegResponseDto extends UserDto {
+    private String firstname;
+    private String lastname;
+    private String secondName;
+    private String username;
+    private String email;
+}
