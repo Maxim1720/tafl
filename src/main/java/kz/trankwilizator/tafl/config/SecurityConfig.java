6b27551ebda4aec4caba2bcc0e450d843abe88a1
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:59:03 +0400

    to inject OncePerRequestFilters as array and rename TemporaryUserDetailsService

--- src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
+++ src/main/java/kz/trankwilizator/tafl/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 @EnableWebSecurity
