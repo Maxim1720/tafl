61d6c950b0ef099c02819904eecbff34598d62ca
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:55:54 +0400

    to change ResponseEntity Body type from AuthToken to ResponseDto in AuthorizationRestController.authorization

--- src/main/java/kz/trankwilizator/tafl/controller/auth/TemporaryUserAuthRestController.java
+++ src/main/java/kz/trankwilizator/tafl/controller/auth/TemporaryUserAuthRestController.java
@@ -1,9 +1,10 @@
 package kz.trankwilizator.tafl.controller.auth;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import kz.trankwilizator.tafl.dto.response.ResponseDto;
 import kz.trankwilizator.tafl.service.auth.AuthService;
-import kz.trankwilizator.tafl.dto.AuthToken;
-import kz.trankwilizator.tafl.dto.auth.TempUserAuthDto;
+import kz.trankwilizator.tafl.dto.auth.TemporaryUserAuthDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/temp")
-public class TemporaryUserAuthRestController extends AuthorizationRestController<TempUserAuthDto> {
-    public TemporaryUserAuthRestController(AuthService<TempUserAuthDto> authService) {
+public class TemporaryUserAuthRestController extends AuthorizationRestController<TemporaryUserAuthDto> {
+    public TemporaryUserAuthRestController(AuthService<TemporaryUserAuthDto> authService) {
         super(authService);
     }
     @Override
-    public ResponseEntity<AuthToken> authorization(@RequestBody TempUserAuthDto user) {
-        return super.authorization(user);
+    public ResponseEntity<ResponseDto> authorization(@RequestBody TemporaryUserAuthDto user, HttpServletRequest request) {
+        return super.authorization(user, request);
     }
 }
