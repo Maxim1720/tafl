5c11b651b59ff1040bb42be0c7add5569a68d5d9
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:53:24 +0400

    to add @RepositoryRestResource(path = "permissions") to PermissionRepository

--- src/main/java/kz/trankwilizator/tafl/dao/role/PermissionRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/role/PermissionRepository.java
@@ -2,8 +2,10 @@ package kz.trankwilizator.tafl.dao.role;
 
 import kz.trankwilizator.tafl.entity.role.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
+@RepositoryRestResource(path = "permissions")
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
 }
