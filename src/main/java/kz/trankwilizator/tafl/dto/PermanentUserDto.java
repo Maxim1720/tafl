3bcb6282e0868eb84cc86b15c75a65e37b486649
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:49:48 +0400

    to rename AbsUserDto.java to UserDto and add @NoArgsConstructor and @AllArgsConstructor

--- src/main/java/kz/trankwilizator/tafl/dto/PermanentUserDto.java
+++ src/main/java/kz/trankwilizator/tafl/dto/PermanentUserDto.java
@@ -1,11 +1,15 @@
 package kz.trankwilizator.tafl.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class PermanentUserDto extends AbsUserDto {
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermanentUserDto extends UserDto {
     private String firstname;
     private String lastname;
     private String secondName;
