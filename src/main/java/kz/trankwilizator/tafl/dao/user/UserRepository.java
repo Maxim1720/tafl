2b45bb4bb5372213e077918b9bf048f84717efe3
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:51:38 +0400

    to make UserRepository @NoRepositoryBean and add resource path

--- src/main/java/kz/trankwilizator/tafl/dao/user/UserRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/user/UserRepository.java
@@ -4,10 +4,14 @@ import kz.trankwilizator.tafl.entity.user.PermanentUser;
 import kz.trankwilizator.tafl.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
+@NoRepositoryBean
+@RestResource(path = "users")
 public interface UserRepository<U extends User> extends JpaRepository<U, Long>, JpaSpecificationExecutor<PermanentUser> {
     Optional<U> findByUsernameIgnoreCase(@NonNull String login);
 
