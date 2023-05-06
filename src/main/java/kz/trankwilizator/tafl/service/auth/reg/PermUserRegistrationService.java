a60871531ff5c13b0f23b82f8e4d035bcdfb8948
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:41:56 +0400

    to check existing before registration user in PermUserRegistrationService.createUser

--- src/main/java/kz/trankwilizator/tafl/service/auth/reg/PermUserRegistrationService.java
+++ src/main/java/kz/trankwilizator/tafl/service/auth/reg/PermUserRegistrationService.java
@@ -5,6 +5,7 @@ import kz.trankwilizator.tafl.dao.user.UserRepository;
 import kz.trankwilizator.tafl.dao.role.RoleRepository;
 import kz.trankwilizator.tafl.dto.PermanentUserDto;
 import kz.trankwilizator.tafl.entity.user.PermanentUser;
+import kz.trankwilizator.tafl.exception.ExistsException;
 import kz.trankwilizator.tafl.mapper.PermanentUserMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -27,11 +28,18 @@ public class PermUserRegistrationService implements PermanentUserRegistrationSer
 
     @Override
     public PermanentUserDto createUser(PermanentUserDto user) {
+        checkExisting(user);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         PermanentUser permanent = userMapper.toEntity(user);
         permanent.setBalance(0.0);
         permanent.setRole(roleRepository.findByNameIgnoreCase("user").orElseThrow(()->new EntityExistsException("Role doesn't exists")));
         return userMapper.toDto(userRepository.save(permanent));
+    }
+
+    private void checkExisting(PermanentUserDto user){
+        userRepository.findByUsernameIgnoreCase(user.getUsername()).ifPresent((u)-> {
+            throw new ExistsException(String.format("%s already exists",u.getUsername()), user);
+        });
     }
 }
