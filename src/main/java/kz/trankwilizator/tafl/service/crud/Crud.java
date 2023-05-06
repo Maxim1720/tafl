8ee0eafe6fe911457200e0be86564fc06d5b665b
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:25:02 +0400

    to add Crud.exists, Crud.existsByUsername

--- src/main/java/kz/trankwilizator/tafl/service/crud/Crud.java
+++ src/main/java/kz/trankwilizator/tafl/service/crud/Crud.java
@@ -3,4 +3,7 @@ package kz.trankwilizator.tafl.service.crud;
 public interface Crud<E> {
     E getById(Long id);
     E save(E e);
+
+    boolean exists(E e);
+    boolean existsByUsername(String username);
 }
