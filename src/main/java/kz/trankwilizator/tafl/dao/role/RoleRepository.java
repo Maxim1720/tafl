a2a8291c80e3057e333d8f91a359a9457a11cee4
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:53:01 +0400

    to add @RepositoryRestResource(path = "roles") to RoleRepository

--- src/main/java/kz/trankwilizator/tafl/dao/role/RoleRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/role/RoleRepository.java
@@ -3,9 +3,11 @@ package kz.trankwilizator.tafl.dao.role;
 
 import kz.trankwilizator.tafl.entity.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "roles")
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByNameIgnoreCase(String name);
 }
