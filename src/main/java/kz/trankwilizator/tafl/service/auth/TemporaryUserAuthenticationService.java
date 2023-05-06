086f564cd5fb159b337cea6c2cbc38964768f556
maksim1708 <musiev_maksim@mail.ru>
2023-05-06 16:37:10 +0400

    to rename TempUserAuthDto to TemporaryUserAuthDto

--- src/main/java/kz/trankwilizator/tafl/service/auth/TemporaryUserAuthenticationService.java
+++ src/main/java/kz/trankwilizator/tafl/service/auth/TemporaryUserAuthenticationService.java
@@ -2,7 +2,7 @@ package kz.trankwilizator.tafl.service.auth;
 
 import kz.trankwilizator.tafl.entity.user.TemporaryUser;
 import kz.trankwilizator.tafl.service.crud.JwtTokenCrudService;
-import kz.trankwilizator.tafl.dto.auth.TempUserAuthDto;
+import kz.trankwilizator.tafl.dto.auth.TemporaryUserAuthDto;
 import kz.trankwilizator.tafl.security.JwtTokenProvider;
 import kz.trankwilizator.tafl.service.util.JwtTokenCreator;
 import kz.trankwilizator.tafl.service.crud.user.UserCrudService;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Log
-public class TemporaryUserAuthenticationService extends AuthenticationService<TempUserAuthDto, TemporaryUser> {
+public class TemporaryUserAuthenticationService extends AuthenticationService<TemporaryUserAuthDto, TemporaryUser> {
     public TemporaryUserAuthenticationService(JwtTokenCrudService jwtTokenCrudService,
                                               JwtTokenCreator jwtTokenCreator,
                                               JwtTokenProvider jwtTokenProvider,
