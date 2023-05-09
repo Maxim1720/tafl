474c83af0c03c037b0ccd6b43111e5fb1d26be8e
maksim1708 <musiev_maksim@mail.ru>
2023-05-09 16:56:20 +0400

    to remove unused @RepositoryRestResource from TaflApplication

--- src/main/java/kz/trankwilizator/tafl/TaflApplication.java
+++ src/main/java/kz/trankwilizator/tafl/TaflApplication.java
@@ -3,11 +3,9 @@ package kz.trankwilizator.tafl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@RepositoryRestResource
 public class TaflApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaflApplication.class, args);
