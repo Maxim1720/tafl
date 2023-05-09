ffa240e0939faac809383d6bd0ba7f5d503223bc
maksim1708 <musiev_maksim@mail.ru>
2023-05-09 17:56:30 +0400

    to change users' resource path

--- src/main/java/kz/trankwilizator/tafl/dao/user/PermanentUserRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/user/PermanentUserRepository.java
@@ -3,7 +3,7 @@ package kz.trankwilizator.tafl.dao.user;
 import kz.trankwilizator.tafl.entity.user.PermanentUser;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "permanents")
+@RepositoryRestResource(path = "permanent-users")
 public interface PermanentUserRepository extends UserRepository<PermanentUser>{
 
 }
--- src/main/java/kz/trankwilizator/tafl/dao/user/TemporaryUserRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/user/TemporaryUserRepository.java
@@ -3,7 +3,7 @@ package kz.trankwilizator.tafl.dao.user;
 import kz.trankwilizator.tafl.entity.user.TemporaryUser;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "temporaries")
+@RepositoryRestResource(path = "temporary-users")
 public interface TemporaryUserRepository extends UserRepository<TemporaryUser>{
 
 }
