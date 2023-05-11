a27714e7e04f4742eaffb770c245c0c92f57bbd1
Максим Мусиев <56740030+Maxim1720@users.noreply.github.com>
2023-05-11 23:22:58 +0500

    Update Computer.java
    Fix import of Zone

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/Computer.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/Computer.java
@@ -3,6 +3,7 @@ package kz.trankwilizator.tafl.entity.launchable;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import kz.trankwilizator.tafl.entity.Zone;
 
 @Table(name = "computer")
 @Entity
