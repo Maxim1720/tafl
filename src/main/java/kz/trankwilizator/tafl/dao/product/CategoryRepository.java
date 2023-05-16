06792e0184a34ad6892c3ea93a84ceedf456436a
Максим Мусиев <56740030+Maxim1720@users.noreply.github.com>
2023-05-16 13:27:46 +0500

    Update CategoryRepository.java

--- src/main/java/kz/trankwilizator/tafl/dao/product/CategoryRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/product/CategoryRepository.java
@@ -1,8 +1,8 @@
-package kz.trankwilizator.dao.product;
+package kz.trankwilizator.tafl.dao.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import kz.trankwilizator.entity.product.Category;
+import kz.trankwilizator.tafl.entity.product.Category;
 
 @RepositoryRestResource
 public interface CategoryRepository extends JpaRepository<Category, Long> {
