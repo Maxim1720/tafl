12c438e3291d3125e75659154bb21518ed42d6c2
maksim1708 <musiev_maksim@mail.ru>
2023-05-20 18:00:54 +0400

    to set path of DeviceStatusRepository.java

--- src/main/java/kz/trankwilizator/tafl/dao/launchable/device/DeviceStatusRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/launchable/device/DeviceStatusRepository.java
@@ -2,10 +2,12 @@ package kz.trankwilizator.tafl.dao.launchable.device;
 
 import kz.trankwilizator.tafl.entity.launchable.device.DeviceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "device-statuses")
 public interface DeviceStatusRepository extends JpaRepository<DeviceStatus, Long> {
     Optional<DeviceStatus> findByNameIgnoreCase(@NonNull String name);
 
