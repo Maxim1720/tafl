4e4a0dc648072926382799c6b2758b9ef2b711be
maksim1708 <musiev_maksim@mail.ru>
2023-05-20 13:41:03 +0400

    to mv DeviceRepository.java

--- src/main/java/kz/trankwilizator/tafl/dao/launchable/DeviceRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/launchable/DeviceRepository.java
@@ -1,6 +1,6 @@
 package kz.trankwilizator.tafl.dao.launchable;
 
-import kz.trankwilizator.tafl.entity.launchable.Device;
+import kz.trankwilizator.tafl.entity.launchable.device.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
