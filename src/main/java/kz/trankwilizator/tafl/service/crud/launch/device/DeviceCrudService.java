59bf108260db394be95aed25f23fe811ce9a6845
maksim1708 <musiev_maksim@mail.ru>
2023-05-20 16:58:01 +0400

    to mv DeviceCrudService and DeviceStatusCrudService

--- src/main/java/kz/trankwilizator/tafl/service/crud/launch/device/DeviceCrudService.java
+++ src/main/java/kz/trankwilizator/tafl/service/crud/launch/device/DeviceCrudService.java
@@ -1,6 +1,6 @@
-package kz.trankwilizator.tafl.service.crud.device;
+package kz.trankwilizator.tafl.service.crud.launch.device;
 
-import kz.trankwilizator.tafl.dao.launchable.DeviceRepository;
+import kz.trankwilizator.tafl.dao.launchable.device.DeviceRepository;
 import kz.trankwilizator.tafl.entity.launchable.device.Device;
 import kz.trankwilizator.tafl.service.crud.CrudService;
 import org.springframework.stereotype.Service;
