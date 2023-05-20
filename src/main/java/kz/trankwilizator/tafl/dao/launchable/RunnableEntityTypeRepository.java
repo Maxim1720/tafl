ac31061d825f462a1b691b3629d0d36bda79fc73
maksim1708 <musiev_maksim@mail.ru>
2023-05-20 18:01:09 +0400

    to create RunnableEntityTypeRepository.java

---
+++ src/main/java/kz/trankwilizator/tafl/dao/launchable/RunnableEntityTypeRepository.java
@@ -0,0 +1,10 @@
+package kz.trankwilizator.tafl.dao.launchable;
+
+import kz.trankwilizator.tafl.entity.launchable.RunnableEntityType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "run-entity-type")
+public interface RunnableEntityTypeRepository extends JpaRepository<RunnableEntityType, Long> {
+
+}
