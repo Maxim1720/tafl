e1d2a0892072cebfad74dc14eb83f8d1e05e6399
maksim1708 <musiev_maksim@mail.ru>
2023-05-20 17:00:10 +0400

    to add LaunchRepository.findByRunnableEntity

--- src/main/java/kz/trankwilizator/tafl/dao/launchable/LaunchRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/launchable/LaunchRepository.java
@@ -1,10 +1,14 @@
-package kz.trankwilizator.tafl.dao.user;
+package kz.trankwilizator.tafl.dao.launchable;
 
 import kz.trankwilizator.tafl.entity.launchable.Launch;
+import kz.trankwilizator.tafl.entity.launchable.RunnableEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(path = "launches")
 public interface LaunchRepository extends JpaRepository<Launch, Long> {
+    Optional<Launch> findByRunnableEntity(RunnableEntity runnableEntity);
 
 }
