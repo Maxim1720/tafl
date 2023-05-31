8dbf9909f7badea54a164e9ecb149f55187b0e60
maksim1708 <musiev_maksim@mail.ru>
2023-06-01 01:12:54 +0400

    to add Schedule relation to Tariff

--- src/main/java/kz/trankwilizator/tafl/entity/zone/tariff/Tariff.java
+++ src/main/java/kz/trankwilizator/tafl/entity/zone/tariff/Tariff.java
@@ -2,6 +2,9 @@ package kz.trankwilizator.tafl.entity.zone.tariff;
 
 
 import jakarta.persistence.*;
+import kz.trankwilizator.tafl.entity.schedule.Schedule;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
@@ -17,6 +20,8 @@ public class Tariff {
 
     @Column(nullable = false)
     private String name;
-    private Date startOfAction;
-    private Date endOfAction;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Schedule schedule;
 }
