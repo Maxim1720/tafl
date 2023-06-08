9977ab6f225e83c240ca2d7ec5786fc99937657b
Maxim Musiev <musiev_maksim@mail.ru>
2023-06-09 03:46:40 +0400

    to permit access of Product data to cashier and admin

--- src/main/java/kz/trankwilizator/tafl/dao/product/ProductRepository.java
+++ src/main/java/kz/trankwilizator/tafl/dao/product/ProductRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import kz.trankwilizator.tafl.entity.product.Product;
 import org.springframework.security.access.annotation.Secured;
 
-@Secured("MODERATOR")
+@Secured({"MODERATOR", "CASHIER", "ADMIN"})
 @RepositoryRestResource(path="products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
