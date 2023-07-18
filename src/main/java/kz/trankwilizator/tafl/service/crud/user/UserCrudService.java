169e72d22827b7c60dc92da547d0570d538fb309
Максим Мусиев <56740030+Maxim1720@users.noreply.github.com>
2023-07-18 20:33:40 +0500

    to realize Saver in UserCrudService

--- src/main/java/kz/trankwilizator/tafl/service/crud/user/UserCrudService.java
+++ src/main/java/kz/trankwilizator/tafl/service/crud/user/UserCrudService.java
@@ -4,12 +4,13 @@ import jakarta.persistence.EntityNotFoundException;
 import kz.trankwilizator.tafl.dao.user.UserRepository;
 import kz.trankwilizator.tafl.entity.user.User;
 import kz.trankwilizator.tafl.service.crud.Crud;
+import kz.trankwilizator.tafl.service.crud.Saver;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
 import java.util.Optional;
 
-public abstract class UserCrudService<U extends User> implements Crud<U> {
+public abstract class UserCrudService<U extends User> implements Crud<U>, Saver<U> {
     private final UserRepository<U> repository;
 
     protected UserCrudService(UserRepository<U> repository) {
@@ -24,6 +25,11 @@ public abstract class UserCrudService<U extends User> implements Crud<U> {
     @Override
     public U save(U u) {
         return repository.save(u);
+    }
+
+    @Override
+    public Collection<U> saveAll(Collection<U> values){
+        return repository.saveAll(values);
     }
 
     public U getByUsername(String username){
