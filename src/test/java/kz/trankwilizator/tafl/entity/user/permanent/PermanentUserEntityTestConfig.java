fbbd266d90adaa023ccf41e0d2e7dc7f9f1e6315
trankwilizator <musiev17@gmail.com>
2023-09-16 15:05:33 +0300

    to add static INSTANCE to PermanentUserEntityTestConfig

--- src/test/java/kz/trankwilizator/tafl/entity/user/permanent/PermanentUserEntityTestConfig.java
+++ src/test/java/kz/trankwilizator/tafl/entity/user/permanent/PermanentUserEntityTestConfig.java
@@ -2,6 +2,7 @@ package kz.trankwilizator.tafl.entity.user.permanent;
 
 import jakarta.persistence.EntityNotFoundException;
 import kz.trankwilizator.tafl.dao.role.RoleRepository;
+import kz.trankwilizator.tafl.entity.role.Role;
 import kz.trankwilizator.tafl.entity.user.PermanentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -27,6 +28,22 @@ public class PermanentUserEntityTestConfig {
         permanentUser.setEnabled(false);
         permanentUser.setRole(roleRepository.findByNameIgnoreCase("PERMANENT").orElseThrow(EntityNotFoundException::new));
         return permanentUser;
+    }
+
+    public static PermanentUser INSTANCE;
+
+    static {
+        INSTANCE = new PermanentUser();
+        INSTANCE.setUsername("test username");
+        INSTANCE.setFirstname("test firstname");
+        INSTANCE.setLastname("test lastname");
+        INSTANCE.setSecondName("test second name");
+        INSTANCE.setEmail("test@test.test");
+        INSTANCE.setDiscount(0.0);
+        INSTANCE.setBalance(0.0);
+        INSTANCE.setPassword("$2a$12$mgn/TIcF3e/SIKb3o8uoIuKPxAOlQwRiK3QVHDs3UX8Os3XUN468W".toCharArray());
+        INSTANCE.setEnabled(false);
+        INSTANCE.setRole(new Role());
     }
 
 }
