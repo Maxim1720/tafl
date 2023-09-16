5fbd01fed10f532b0c79f73b303a4e6386c8e4bd
trankwilizator <musiev17@gmail.com>
2023-09-16 14:41:42 +0300

    to add Constraint Violations handling method in BadOperationControllerAdvice.java

--- src/main/java/kz/trankwilizator/tafl/exception/handling/BadOperationControllerAdvice.java
+++ src/main/java/kz/trankwilizator/tafl/exception/handling/BadOperationControllerAdvice.java
@@ -1,6 +1,7 @@
 package kz.trankwilizator.tafl.exception.handling;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import kz.trankwilizator.tafl.dto.error.ErrorDto;
 import kz.trankwilizator.tafl.exception.BadOperationException;
 import org.springframework.http.HttpStatus;
@@ -8,11 +9,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 @ControllerAdvice
 public class BadOperationControllerAdvice {
 
     @ExceptionHandler(BadOperationException.class)
     public ResponseEntity<ErrorDto> handle(BadOperationException e, HttpServletRequest request){
         return CrudControllerAdvice.errorResponse(HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI(), e.getBody());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDto> handle(ConstraintViolationException e, HttpServletRequest request) throws IOException {
+        String problem = Arrays.toString(e.getConstraintViolations().stream()
+                .map(c -> c.getRootBeanClass()
+                        .getSimpleName()
+                        + "." + c.getPropertyPath()
+                        + " "
+                        + c.getMessage()
+                        + ": "
+                        + c.getInvalidValue()
+                        + ";")
+                .toArray()).replace("[","").replace("]","\n");
+
+        return CrudControllerAdvice.errorResponse(
+                HttpStatus.BAD_REQUEST, problem, request.getRequestURI(),
+                Arrays.toString(request.getInputStream().readAllBytes())
+        );
     }
 }
