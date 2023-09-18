638f12ff47ef4544c4dafa6a47a6f947cbd8be4f
Maxim1720 <musiev17@gmail.com>
2023-09-18 10:34:27 +0300

    to create abstract ValidationTest.java

---
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/ValidationTest.java
@@ -0,0 +1,26 @@
+package kz.trankwilizator.tafl.entity.validation;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+
+
+public abstract class ValidationTest<E> {
+    @Getter
+    private E entity;
+    private final EntityPropertyValidator<E> entityPropertyValidator;
+    public ValidationTest(){
+        entityPropertyValidator = new EntityPropertyValidator<>();
+    }
+    public void setUpEntity(){
+        this.entity = createInstance();
+    }
+    protected abstract E createInstance();
+
+    public void whenValidate_thenExistsConstraintViolation(String propertyName, boolean exists){
+        Assertions.assertEquals(exists, entityPropertyValidator.validate(getEntity(),propertyName));
+    }
+
+    public boolean hasConstraintViolations(E e, String propertyName){
+        return entityPropertyValidator.validate(e,propertyName);
+    }
+}
