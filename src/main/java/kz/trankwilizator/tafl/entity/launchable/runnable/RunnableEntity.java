fefdf5e03df7b99a1889955ae4f3b6eed0eb01f4
musiev1720 <musiev17@gmail.com>
2023-09-19 18:04:15 +0300

    to set @AllArgsConstructor        @NoArgsConstructor        @SuperBuilder(toBuilder = true) on RunnableEntity

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/runnable/RunnableEntity.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/runnable/RunnableEntity.java
@@ -1,14 +1,20 @@
 package kz.trankwilizator.tafl.entity.launchable.runnable;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
 public abstract class RunnableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
