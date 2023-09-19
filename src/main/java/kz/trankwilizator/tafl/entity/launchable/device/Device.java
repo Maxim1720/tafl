3383d5da4e0617ccee78460d0f87d63e1e7ed0dd
musiev1720 <musiev17@gmail.com>
2023-09-19 11:16:18 +0300

    to set constraints to Device fields

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/device/Device.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/device/Device.java
@@ -1,6 +1,8 @@
 package kz.trankwilizator.tafl.entity.launchable.device;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import kz.trankwilizator.tafl.entity.launchable.runnable.RunnableEntity;
 import kz.trankwilizator.tafl.entity.zone.Zone;
 import lombok.*;
@@ -17,13 +19,17 @@ import java.io.Serializable;
 public class Device extends RunnableEntity implements Serializable {
 
 
+  @NotNull
   @ManyToOne
   @JoinColumn(name = "zone_id", nullable = false)
   private Zone zone;
 
+  @Positive
+  @NotNull
   @Column(unique = true, nullable = false)
   private Long number;
 
+  @NotNull
   @ManyToOne
   @JoinColumn(name = "device_status_id", nullable = false)
   private DeviceStatus deviceStatus;
