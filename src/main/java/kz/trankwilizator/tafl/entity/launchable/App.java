bdbc1b28d126e90422ecbd6e4ac3f4a40cdee793
musiev1720 <musiev17@gmail.com>
2023-09-19 18:05:24 +0300

    to set constraints on App.name

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/App.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/App.java
@@ -1,6 +1,8 @@
 package kz.trankwilizator.tafl.entity.launchable;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import kz.trankwilizator.tafl.entity.launchable.runnable.RunnableEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,4 +19,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 public class App extends RunnableEntity {
 
+    @NotBlank
+    @Size(min = 1, max = 75)
+    @Column(length = 75, nullable = false)
+    private String name;
 }
