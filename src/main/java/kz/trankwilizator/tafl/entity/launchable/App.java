8a28851ec8344675524263b240407d595b37b211
musiev1720 <musiev17@gmail.com>
2023-09-19 18:00:33 +0300

    to set @SuperBuilder(toBuilder = true)        @AllArgsConstructor        @NoArgsConstructor on App

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/App.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/App.java
@@ -2,13 +2,19 @@ package kz.trankwilizator.tafl.entity.launchable;
 
 import jakarta.persistence.*;
 import kz.trankwilizator.tafl.entity.launchable.runnable.RunnableEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "app")
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class App extends RunnableEntity {
 
 }
