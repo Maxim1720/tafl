763e21332f7967244f7132d86b6b8bc341576410
musiev1720 <musiev17@gmail.com>
2023-09-19 13:27:58 +0300

    to set constraints to JwtToken fields

--- src/main/java/kz/trankwilizator/tafl/entity/JwtToken.java
+++ src/main/java/kz/trankwilizator/tafl/entity/JwtToken.java
@@ -1,6 +1,10 @@
 package kz.trankwilizator.tafl.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kz.trankwilizator.tafl.entity.user.User;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -22,10 +26,13 @@ public class JwtToken {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotNull
     @ManyToOne(targetEntity = User.class, cascade = {CascadeType.DETACH, CascadeType.MERGE})
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Size(min = 5, max = 250)
+    @NotBlank
     @Column(length = 250, unique = true, nullable = false)
     private String token;
 
@@ -33,5 +40,7 @@ public class JwtToken {
     @Column(nullable = false)
     private Date createdAt;
 
+    @NotNull
+    @Future
     private Date expiryAt;
 }
