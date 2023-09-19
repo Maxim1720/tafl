5295efdefb90bb818185b2216be1a30505dc17f0
musiev1720 <musiev17@gmail.com>
2023-09-19 16:07:03 +0300

    to set @Builder(toBuilder = true)        @NoArgsConstructor        @AllArgsConstructor        @ToString on Tariff class

--- src/main/java/kz/trankwilizator/tafl/entity/zone/tariff/Tariff.java
+++ src/main/java/kz/trankwilizator/tafl/entity/zone/tariff/Tariff.java
@@ -3,13 +3,16 @@ package kz.trankwilizator.tafl.entity.zone.tariff;
 
 import jakarta.persistence.*;
 import kz.trankwilizator.tafl.entity.schedule.Schedule;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "tariffs")
 @Getter
 @Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Tariff {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
