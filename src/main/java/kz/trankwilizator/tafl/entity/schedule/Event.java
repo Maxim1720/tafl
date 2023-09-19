092aeebcbc595f015d45eed82f18641014e36909
musiev1720 <musiev17@gmail.com>
2023-09-19 18:02:54 +0300

    to set constraints on Event fields

--- src/main/java/kz/trankwilizator/tafl/entity/schedule/Event.java
+++ src/main/java/kz/trankwilizator/tafl/entity/schedule/Event.java
@@ -1,6 +1,9 @@
 package kz.trankwilizator.tafl.entity.schedule;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,12 +23,17 @@ public class Event {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotBlank
+    @Size(min = 1, max = 50)
     @Column(length = 50, nullable = false)
     private String name;
 
+
+    @NotBlank
     @Column(nullable = false)
     private String description;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "schedule_id")
     private Schedule schedule;
