42b2671f9f8b401690d7683882c357ae0c9e518d
musiev1720 <musiev17@gmail.com>
2023-09-19 11:05:41 +0300

    to add DeviceStatus.name validation constraints

--- src/main/java/kz/trankwilizator/tafl/entity/launchable/device/DeviceStatus.java
+++ src/main/java/kz/trankwilizator/tafl/entity/launchable/device/DeviceStatus.java
@@ -1,6 +1,8 @@
 package kz.trankwilizator.tafl.entity.launchable.device;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +16,8 @@ public class DeviceStatus {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank
+    @Size(min = 1, max = 50)
+    @Column(nullable = false, length = 50)
     private String name;
 }
