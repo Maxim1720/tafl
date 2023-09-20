1fde3d13215d8222ccf9114500880102d41d3c02
musiev1720 <musiev17@gmail.ru>
2023-09-20 13:26:42 +0300

    to migrate type from boolean to ValidationResult

--- src/test/java/kz/trankwilizator/tafl/entity/validation/arguments/provider/schedule/StartLocalTimeArgumentsProvider.java
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/arguments/provider/schedule/StartLocalTimeArgumentsProvider.java
@@ -1,5 +1,6 @@
 package kz.trankwilizator.tafl.entity.validation.arguments.provider.schedule;
 
+import kz.trankwilizator.tafl.entity.validation.ValidationResult;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
@@ -11,11 +12,10 @@ public class StartLocalTimeArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
         return Stream.of(
-                Arguments.of(LocalTime.now(), false),
-                Arguments.of(null, true),
-                Arguments.of(LocalTime.now().plusHours(1), true),
-                Arguments.of(LocalTime.now().minusHours(15), false),
-                Arguments.of(LocalTime.of(23, 0).plusHours(1), false)
+                Arguments.of(LocalTime.now(), ValidationResult.OK),
+                Arguments.of(null, ValidationResult.ERROR),
+                Arguments.of(LocalTime.now().plusMinutes(1), ValidationResult.ERROR),
+                Arguments.of(LocalTime.now().minusMinutes(1), ValidationResult.OK)
         );
     }
 }
