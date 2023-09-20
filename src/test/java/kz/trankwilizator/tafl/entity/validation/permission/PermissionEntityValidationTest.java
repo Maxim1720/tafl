fee573781ace57558ad82344e4034183ed5bdb63
musiev1720 <musiev17@gmail.ru>
2023-09-20 13:27:30 +0300

    to migrate type from boolean to ValidationResult

--- src/test/java/kz/trankwilizator/tafl/entity/validation/permission/PermissionEntityValidationTest.java
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/permission/PermissionEntityValidationTest.java
@@ -1,9 +1,12 @@
 package kz.trankwilizator.tafl.entity.validation.permission;
 
 import kz.trankwilizator.tafl.entity.role.Permission;
+import kz.trankwilizator.tafl.entity.validation.ValidationResult;
 import kz.trankwilizator.tafl.entity.validation.ValidationTest;
+import kz.trankwilizator.tafl.entity.validation.arguments.provider.string.name.ConstantNameArgumentsProvider;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.util.HashSet;
 
@@ -23,60 +26,12 @@ public class PermissionEntityValidationTest extends ValidationTest<Permission> {
         return permission;
     }
 
-    @Test
-    public void givenNullName_whenValidate_thenNamePropertyHasConstraintViolation(){
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation(null, true);
-    }
 
-    @Test
-    public void givenBlankName_whenValidate_thenNamePropertyHasConstraintViolation(){
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("\n\n\n   ", true);
-    }
-
-    @Test
-    public void givenEmptyName_whenValidate_thenNamePropertyHasConstraintViolation(){
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("", true);
-    }
-
-    @Test
-    public void givenNameTEST_TEST_TEST_whenValidate_thenNamePropertyHasNotConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("TEST_TEST_TEST", false);
-    }
-
-    @Test
-    public void givenName_TEST_TEST_TEST_whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("_TEST_TEST_TEST", true);
-    }
-
-    @Test
-    public void givenName_TEST_TEST_TEST__whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("_TEST_TEST_TEST_", true);
-    }
-
-    @Test
-    public void givenName_TEST__TEST_TEST__whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("_TEST__TEST_TEST_", true);
-    }
-
-    @Test
-    public void givenNameTEST_TEST__whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("TEST_TEST_", true);
-    }
-
-    @Test
-    public void givenName_TEST_whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("_TEST", true);
-    }
-
-    @Test
-    public void givenNameTEST__whenValidate_thenNamePropertyHasConstraintViolation() {
-        givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation("TEST_", true);
-    }
-
-    private void givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation(String name, boolean exists){
+    @ParameterizedTest(name = "test {index}: name={0}, result={1}")
+    @ArgumentsSource(ConstantNameArgumentsProvider.class)
+    public void givenPermissionName_whenValidate_thenNamePropertyHasConstraintViolation(String name, ValidationResult result){
         getEntity().setName(name);
-//        validationTester.thenHasConstraintViolations(getEntity(), "name", exists);
-        whenValidate_thenHasConstraintViolation("name", exists);
+        whenValidate_thenHasConstraintViolation("name", result);
     }
 
 }
--- src/test/java/kz/trankwilizator/tafl/entity/validation/product/ProductValidationTest.java
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/product/ProductValidationTest.java
@@ -2,9 +2,10 @@ package kz.trankwilizator.tafl.entity.validation.product;
 
 import kz.trankwilizator.tafl.entity.product.Category;
 import kz.trankwilizator.tafl.entity.product.Product;
-import kz.trankwilizator.tafl.entity.validation.arguments.provider.NameArgumentsProvider;
+import kz.trankwilizator.tafl.entity.validation.ValidationResult;
+import kz.trankwilizator.tafl.entity.validation.arguments.provider.string.name.NameArgumentsProvider;
 import kz.trankwilizator.tafl.entity.validation.ValidationTest;
-import kz.trankwilizator.tafl.entity.validation.arguments.provider.PriceArgumentsProvider;
+import kz.trankwilizator.tafl.entity.validation.arguments.provider.MoneyArgumentsProvider;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -25,24 +26,24 @@ public class ProductValidationTest extends ValidationTest<Product> {
     }
 
     @ParameterizedTest(name = "test {index}, price={0} has violations={1}")
-    @ArgumentsSource(PriceArgumentsProvider.class)
-    public void givenPrice_whenValidate_thenHasConstraintViolations(BigDecimal price, boolean hasCV){
+    @ArgumentsSource(MoneyArgumentsProvider.class)
+    public void givenPrice_whenValidate_thenHasConstraintViolations(BigDecimal price, ValidationResult hasCV){
         getEntity().setPrice(price);
         whenValidate_thenHasConstraintViolation("price", hasCV);
     }
 
-    @ParameterizedTest(name = "test {index}: name={0}, exists={1}")
+    @ParameterizedTest(name = "test {index}: name={0}, result={1}")
     @ArgumentsSource(NameArgumentsProvider.class)
-    public void givenName_whenValidate_thenHasConstraintViolations(String name, boolean exists){
+    public void givenName_whenValidate_thenHasConstraintViolations(String name, ValidationResult result){
         getEntity().setName(name);
-        whenValidate_thenHasConstraintViolation("name", exists);
+        whenValidate_thenHasConstraintViolation("name", result);
     }
 
     @ParameterizedTest
     @ArgumentsSource(CategoryArgumentsProvider.class)
-    public void givenCategory_whenValidate_thenHasConstraintViolations(Category category, boolean exists){
+    public void givenCategory_whenValidate_thenHasConstraintViolations(Category category, ValidationResult result){
         getEntity().setCategory(category);
-        whenValidate_thenHasConstraintViolation("category", exists);
+        whenValidate_thenHasConstraintViolation("category", result);
     }
 
     private static class CategoryArgumentsProvider implements ArgumentsProvider{
@@ -50,8 +51,8 @@ public class ProductValidationTest extends ValidationTest<Product> {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
             return Stream.of(
-                    Arguments.of(null, true),
-                    Arguments.of(new Category(), false)
+                    Arguments.of(null, ValidationResult.ERROR),
+                    Arguments.of(new Category(), ValidationResult.OK)
             );
         }
     }
