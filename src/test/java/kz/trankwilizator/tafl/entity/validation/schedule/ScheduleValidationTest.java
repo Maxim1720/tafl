6fe42d83985b7873886064c96150fbc4dcda7a70
musiev1720 <musiev17@gmail.ru>
2023-09-20 13:26:51 +0300

    to migrate type from boolean to ValidationResult

--- src/test/java/kz/trankwilizator/tafl/entity/validation/schedule/ScheduleValidationTest.java
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/schedule/ScheduleValidationTest.java
@@ -1,6 +1,7 @@
 package kz.trankwilizator.tafl.entity.validation.schedule;
 
 import kz.trankwilizator.tafl.entity.schedule.Schedule;
+import kz.trankwilizator.tafl.entity.validation.ValidationResult;
 import kz.trankwilizator.tafl.entity.validation.ValidationTest;
 import kz.trankwilizator.tafl.entity.validation.arguments.provider.schedule.EndLocalTimeArgumentsProvider;
 import kz.trankwilizator.tafl.entity.validation.arguments.provider.schedule.StartLocalTimeArgumentsProvider;
@@ -19,17 +20,17 @@ public class ScheduleValidationTest extends ValidationTest<Schedule> {
                 .build();
     }
 
-    @ParameterizedTest(name = "test {index}: start time={0}, exists={1}")
+    @ParameterizedTest(name = "test {index}: start time={0}, result={1}")
     @ArgumentsSource(StartLocalTimeArgumentsProvider.class)
-    public void givenStartTime_whenValidate_thenHasViolations(LocalTime start, boolean exists){
+    public void givenStartTime_whenValidate_thenHasViolations(LocalTime start, ValidationResult result){
         getEntity().setStartTime(start);
-        whenValidate_thenHasConstraintViolation("startTime", exists);
+        whenValidate_thenHasConstraintViolation("startTime", result);
     }
 
-    @ParameterizedTest(name = "test {index}: end time = {0}, exists = {1}")
+    @ParameterizedTest(name = "test {index}: end time = {0}, result = {1}")
     @ArgumentsSource(EndLocalTimeArgumentsProvider.class)
-    public void givenEndTime_whenValidate_thenHasViolations(LocalTime end, boolean exists){
+    public void givenEndTime_whenValidate_thenHasViolations(LocalTime end, ValidationResult result){
         getEntity().setEndTime(end);
-        whenValidate_thenHasConstraintViolation("endTime", exists);
+        whenValidate_thenHasConstraintViolation("endTime", result);
     }
 }
