9120c41672f653f777a2f228f0098876c1367ece
musiev1720 <musiev17@gmail.ru>
2023-09-20 13:27:07 +0300

    to migrate type from boolean to ValidationResult

--- src/test/java/kz/trankwilizator/tafl/entity/validation/role/RoleEntityValidationTest.java
+++ src/test/java/kz/trankwilizator/tafl/entity/validation/role/RoleEntityValidationTest.java
@@ -1,10 +1,14 @@
 package kz.trankwilizator.tafl.entity.validation.role;
 
+import kz.trankwilizator.tafl.entity.validation.ValidationResult;
 import kz.trankwilizator.tafl.entity.validation.ValidationTest;
 import kz.trankwilizator.tafl.entity.role.Role;
 import kz.trankwilizator.tafl.entity.config.role.RoleTestConfig;
+import kz.trankwilizator.tafl.entity.validation.arguments.provider.string.name.ConstantNameArgumentsProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -27,11 +31,6 @@ public class RoleEntityValidationTest extends ValidationTest<Role> {
     }
 
     @Test
-    public void givenNullName_whenValidate_thenConstraintViolationsNotEmpty(){
-        validateName(null, false);
-    }
-
-    @Test
     public void givenNullPermissions_whenValidate_thenConstraintViolationsNotEmpty(){
         getEntity().setPermissions(null);
         whenValidate_thenHasConstraintViolation("permissions", true);
@@ -42,65 +41,12 @@ public class RoleEntityValidationTest extends ValidationTest<Role> {
         Arrays.stream(Role.class.getFields()).forEach(f-> whenValidate_thenHasConstraintViolation(f.getName(), false));
     }
 
-    @Test
-    public void givenShortRoleName_whenValidate_thenConstraintViolationsNotEmpty(){
-        validateName("R", false);
-    }
 
-    @Test
-    public void givenTooLongRoleName_whenValidate_thenConstraintViolationsNotEmpty(){
-        String roleName = "R".repeat(76);
-        validateName(roleName, false);
-    }
 
-    @Test
-    public void givenR_Name_whenValidate_thenConstraintViolationsNotEmpty(){
-        validateName("R_", false);
-    }
-
-    @Test
-    public void givenr_Name_whenValidate_thenConstraintViolationsNotEmpty(){
-        validateName("r_",false);
-    }
-
-    @Test
-    public void givenTEST_PERMANENT_USER_whenValidate_thenConstraintViolationsIsEmpty(){
-        validateName("TEST_PERMANENT_USER", true);
-    }
-
-    @Test
-    public void givenName_TEST__whenValidate_thenConstraintViolationsExists(){
-        validateName("_TEST__", false);
-    }
-
-    @Test
-    public void givenNameTEST__TEST_whenValidate_thenConstraintViolationsExists(){
-        validateName("TEST__TEST", false);
-    }
-
-    @Test
-    public void givenNameTEST_TEST__whenValidate_thenConstraintViolationsExists(){
-        validateName("TEST_TEST_", false);
-    }
-
-    @Test
-    public void givenNameTEST_TEST_TEST_whenValidate_thenConstraintViolationsNotExists(){
-        validateName("TEST_TEST_TEST", true);
-    }
-
-    @Test
-    public void givenBlankRoleName_whenValidate_thenConstraintViolationsIsNotEmpty(){
-        validateName("   \n\t\r", false);
-    }
-
-    @Test
-    public void givenEmptyRoleName_whenValidate_thenConstraintViolationsIsEmpty(){
-        validateName("", false);
-    }
-
-    private void validateName(String name, boolean constraintViolationsIsEmpty){
+    @ParameterizedTest(name = "test {index}: name={0}, result={1}")
+    @ArgumentsSource(ConstantNameArgumentsProvider.class)
+    public void givenName_whenValidate_thenHasConstraintViolations(String name, ValidationResult result){
         getEntity().setName(name);
-//        new ValidationTester<Role>().thenHasConstraintViolations(getEntity(), "name", !constraintViolationsIsEmpty);
-        whenValidate_thenHasConstraintViolation("name",!constraintViolationsIsEmpty);
+        whenValidate_thenHasConstraintViolation("name", result);
     }
 }
